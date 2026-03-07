c7ea257f621f33674d1c3b0114949d534b082eec
detyse <dts.sino@foxmail.com>
2026-03-07 21:45:05 +0800

    rename nanobot

---
+++ agent/__main__.py
@@ -0,0 +1,6 @@
+"""nanobot CLI has been merged into manobot. Use 'manobot' command instead."""
+
+import sys
+
+print("nanobot CLI has been merged into manobot. Use 'manobot' instead.", file=sys.stderr)
+sys.exit(1)
